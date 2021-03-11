e8133f139475874c81fc90c074e57515488a3a31
AlerHugu3s <axiasiming@gmail.com>
2021-03-11 17:50:34 +0800

    Implements of EventSystem

--- Ancestor/src/Ancestor/Application.cpp
+++ Ancestor/src/Ancestor/Application.cpp
@@ -1,15 +1,19 @@
 #include "acpch.h"
 #include "Application.h"
 
-#include "Ancestor/Events/ApplicationEvent.h"
 #include "Ancestor/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Ancestor {
+
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1) 
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
+
 	}
 
 	Application::~Application()
@@ -21,9 +25,42 @@ namespace Ancestor {
 	{
 		while (m_Running)
 		{
-			glClearColor(1, 0, 1, 1);
+			glClearColor(color.red, color.green, color.blue, color.alpha);
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+		dispatcher.Dispatch<MouseButtonPressedEvent>(BIND_EVENT_FN(OnMouseClick));
+		AC_CORE_TRACE("{0}", e);
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
+	}
+
+	bool Application::OnMouseClick(MouseButtonPressedEvent& e)
+	{
+		int button = e.GetButton();
+		switch (button)
+		{
+		case GLFW_MOUSE_BUTTON_1:
+			color.red = 0.0f;
+			color.green = 1.0f;
+			color.blue = 1.0f;
+			return true;
+		case GLFW_MOUSE_BUTTON_2:
+			color.red = 1.0f;
+			color.green = 0.0f;
+			color.blue = 1.0f;
+			return true;
+		}
+		return false;
 	}
 }
--- Ancestor/src/Ancestor/Application.h
+++ Ancestor/src/Ancestor/Application.h
@@ -3,8 +3,22 @@
 #include "Core.h"
 #include "Window.h"
 
+#include "Ancestor/Events/ApplicationEvent.h"
+#include "Ancestor/Events/MouseEvent.h"
+
 
 namespace Ancestor {
+	struct Color
+	{
+		float red = 1.0f;
+		float green = 0.0f;
+		float blue = 1.0f;
+		float alpha = 1.0f;
+
+		Color(float r = 1.0f, float g = 0.0f, float b = 1.0f, float a = 1.0f)
+			:red(r), blue(b), green(g), alpha(a) {}
+	};
+
 	class ANCESTOR_API Application
 	{
 	public:
@@ -12,7 +26,14 @@ namespace Ancestor {
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
 	private:
+		Color color;
+
+		bool OnWindowClose(WindowCloseEvent& e);
+		bool OnMouseClick(MouseButtonPressedEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
